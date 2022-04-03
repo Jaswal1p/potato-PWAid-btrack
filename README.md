b76cbb759a8eedd816aeb3c8c644e75562e0fcdf
Jaswal1p <Jaswal1p@yahoo.com>
2022-04-03 03:44:50 -0400

    added further code in service workers'

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # potato-PWAid-btrack
+
+# npm dependencies: 
+  1. Nodemon: npm install -g nodemon
+  2. Compression: npm install compression
+  3. Mongoose: npm install mongoose
+  4. Morgan (middleware): npm install morgan
+  5. express.js (already installed in starter code): npm install express
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
-const APP_PREFIX = 'BudgetTracker-';     
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
+// const APP_PREFIX = 'potato_PWA-';     
+// const VERSION = 'version_01';
+// const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
     './index.html',
@@ -18,6 +18,10 @@ const FILES_TO_CACHE = [
     './icons/icon-72x72.png',
 ];
 
+const APP_PREFIX = 'potato_PWA-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
 self.addEventListener('install', function (e) {
     e.waitUntil(
       caches.open(CACHE_NAME).then(function (cache) {
@@ -25,6 +29,7 @@ self.addEventListener('install', function (e) {
         return cache.addAll(FILES_TO_CACHE)
       })
     )
+    self.skipWaiting();
   });
 
 self.addEventListener('activate', function (e) {
@@ -45,3 +50,18 @@ self.addEventListener('activate', function (e) {
     )
 });
 
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+                if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url);
+                return request
+            } else {     // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url);
+                return fetch(e.request)
+
+            }
+        })
+    )
+});
