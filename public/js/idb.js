b31211ffaa3c6c05ea1de4e2fc00018ee64e955e
Jaswal1p <Jaswal1p@yahoo.com>
2022-04-02 21:12:26 -0400

    added event listener for onSuccess

--- public/js/idb.js
+++ public/js/idb.js
@@ -9,7 +9,26 @@ const request = indexedDB.open('potato_PWA', 1);
 request.onupgradeneeded = function(event) {
     // save a reference to the database
     const db = event.target.result;
-    
+
     // create an object store (table) called `new_transaction`and set it to have an auto increment primary key of sorts
     db.createObjectStore('new_transaction', { autoIncrement: true}); 
 };
+
+// upon a successful
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from onupgradeneeded event above) or simply established a connection, save reference to db in global variable
+
+    db = event.target.result;
+
+    // check if app is online, if yes run uploadTransaction() function to send all local db data to api
+
+    if (navigator.online) {
+       // uploadTrasaction();
+    }
+};
+
+request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+};
+
